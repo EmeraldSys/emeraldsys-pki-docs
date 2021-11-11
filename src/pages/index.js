31cad1c77ddc7828ebdecbad6d154299667400dc
elementemerald <22267679+elementemerald@users.noreply.github.com>
2021-11-11 00:16:05 -0700

    Updated index.js, index.css and added x509v3.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,24 @@ import "../styles/index.css"
 const DocsPage = () => {
     return (
         <body>
-            <div>
-                Hello World
+            <div className="content">
+                <header className="mainHeader">
+                    <div className="alignHeader">
+                        <img src="https://emeraldsys.github.io/images/company2_banner_white.png" alt="Logo" style={{ height: "45px", marginTop: "15px" }} />
+                    </div>
+                </header>
+                <div className="docsContent">
+                    <span className="titleBold">PKI Documentation</span>
+                    <br />
+                    <br />
+                    <hr size="1" color="#171717" noshade />
+                    <br />
+                    <div className="docsIntro">
+                        <p className="contentLight">This documentation covers about every aspect of the EmeraldSys PKI. If you find something not documented, it is probably still in testing.</p>
+                        <br />
+                        <a href="/x509v3" style={{ color: "#fff", textDecoration: "none" }}>X509v3 API</a>
+                    </div>
+                </div>
             </div>
         </body>
     );
---
+++ src/pages/x509v3.js
@@ -0,0 +1,20 @@
+import * as React from "react"
+
+const X509V3Page = () => {
+    return (
+        <body>
+            <div className="content">
+                <header className="mainHeader">
+                    <div className="alignHeader">
+                        <img src="https://emeraldsys.github.io/images/company2_banner_white.png" alt="Logo" style={{ height: "45px", marginTop: "15px" }} />
+                    </div>
+                </header>
+                <div className="docsContent">
+                    <span className="titleBold">X509v3 API</span>
+                </div>
+            </div>
+        </body>
+    );
+};
+
+export default X509V3Page
