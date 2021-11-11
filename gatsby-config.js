c6e25c00502473c7dcae1b03392bbf01a9639c42
elementemerald <22267679+elementemerald@users.noreply.github.com>
2021-11-11 14:38:20 -0700

    Update site url

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://docs.pki.emeraldsys.xyz",
     title: "EmeraldSys PKI Docs",
   },
   plugins: [
