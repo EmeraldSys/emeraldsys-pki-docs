cfd749abe9907b847b59efd0324a1cb39c8bc768
elementemerald <22267679+elementemerald@users.noreply.github.com>
2022-01-08 01:45:07 -0700

    Update copyright in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import "./styles/Footer.css"
 const Footer = () => {
     return (
         <div className="mainFooter">
-            <p>Copyright (c) 2021 EmeraldSys</p>
+            <p>Copyright (c) 2022 EmeraldSys</p>
         </div>
     );
 };
